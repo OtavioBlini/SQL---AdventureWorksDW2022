a638c29af9869c694adfffa6626a0891d910b848
José Otávio <106994475+OtavioBlini@users.noreply.github.com>
2025-04-03 15:11:31 -0300

    Clientes.sql

--- SQL/Clientes.sql
+++ SQL/Clientes.sql
@@ -2,11 +2,10 @@ USE AdventureWorksDW2022;
 
 -- Clientes por gênero.
 SELECT
-	COUNT(DISTINCT(C.CustomerKey)) AS 'Clientes',
-	C.Gender AS 'Genero'
+	COUNT(DISTINCT(C.CustomerKey)) AS clientes,
+C.Gender AS genero
 FROM FactInternetSales S
-LEFT JOIN DimCustomer C
-ON S.CustomerKey = C.CustomerKey
+LEFT JOIN DimCustomer C ON S.CustomerKey = C.CustomerKey
 GROUP BY C.Gender
 ORDER BY 1;
 
@@ -21,23 +20,22 @@ WITH FaixaEtariaClientes (CustomerKey, FaixaEtaria) AS (
 			WHEN DATEDIFF(YEAR, BirthDate, '2014-01-01') BETWEEN 30 AND 44 THEN '30-44'
 			WHEN DATEDIFF(YEAR, BirthDate, '2014-01-01') BETWEEN 45 AND 59 THEN '45-59'
 			WHEN DATEDIFF(YEAR, BirthDate, '2014-01-01') >= 60 THEN '60+'
-			ELSE 'Desconhecido'
+			ELSE 'desconhecido'
 		END AS FaixaEtaria
 	FROM DimCustomer
 )
 SELECT
-	COUNT(DISTINCT(S.CustomerKey)) AS 'Clientes',
-	FaixaEtaria AS 'Faixa Etária'
+	COUNT(DISTINCT(S.CustomerKey)) AS clientes,
+	FaixaEtaria AS faixa_etaria
 FROM FactInternetSales S
-LEFT JOIN FaixaEtariaClientes F
-ON S.CustomerKey = F.CustomerKey
+LEFT JOIN FaixaEtariaClientes F ON S.CustomerKey = F.CustomerKey
 GROUP BY F.FaixaEtaria
 ORDER BY 2;
 
 -- Cliente novos por ano.
 SELECT 
-	YEAR(DateFirstPurchase) AS Ano, 
-	COUNT(DISTINCT CustomerKey) AS 'Clientes Novos'
+	YEAR(DateFirstPurchase) AS ano, 
+	COUNT(DISTINCT CustomerKey) AS clientes_novos
 FROM DimCustomer
 WHERE DateFirstPurchase IS NOT NULL 
 AND YEAR(DateFirstPurchase) NOT IN (2010, 2014)
